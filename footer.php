b14817a6bdf1189f82d1a8c97f2b059052e6dacc
Roberto Enasoaie <robb110official@gmail.com>
2020-12-21 10:27:37 +0100

    Removed hamburger menu footer

--- footer.php
+++ footer.php
@@ -1,10 +1,12 @@
 <!--Footer-->
     <footer class="page-footer text-center font-small" id="footer">
+        <!--
         <div class="mobile-hamburger-menu animated">
             <div class="whatsapp"><a href="mailto:danilo.b@viverediturismo.com"><i class="fas fa-envelope"></i></a></div>
             <div class="mobile_menu_button"><button class="c-hamburger c-hamburger--htx navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span>toggle menu</span></button></div>
             <div class="book-tel"><a href="tel:0550763804"><i class="fas fa-phone"></i></a></div>
         </div>
+        --!>
     <?php wp_footer(); ?>
     <script>
         (function($) {
